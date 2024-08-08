3d6fcdb58a6a4f1e567f4d23de50e4999931c8bf
unknown <amaldapatrick@gmail.com>
2024-08-08 22:15:25 +0530

    Signin commit-1

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from userauths.models import User
 
+
+class UserAdmin(admin.ModelAdmin):#display users
+    list_display=['username','email']
+
+admin.site.register (User,UserAdmin)
 # Register your models here.
---
+++ userauths/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from userauths.models import User
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Username",
+            "class": "form-control",
+            "required": "required",
+            "pattern": "[a-zA-Z0-9]{3,}",  # Example pattern
+            "title": "Username must be at least 3 characters long and contain only letters and numbers."
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            "placeholder": "Email",
+            "class": "form-control",
+            "required": "required"
+        })
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Password",
+            "class": "form-control",
+            "required": "required",
+            "pattern": "(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}",  # Pattern for strong password
+            "title": "Password must be at least 8 characters long and include uppercase letters, lowercase letters, numbers, and symbols."
+        })
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Confirm Password",
+            "class": "form-control",
+            "required": "required",
+            "minlength": "8"
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
