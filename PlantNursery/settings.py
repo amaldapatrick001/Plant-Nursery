6dd098f3e9fe6553146824c91f9ea012d37d78e9
unknown <amaldapatrick@gmail.com>
2024-10-07 16:01:03 +0530

    Commit on Contact Us and About Page

--- PlantNursery/settings.py
+++ PlantNursery/settings.py
@@ -9,6 +9,26 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
+
+
+GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
+if not GOOGLE_OAUTH_CLIENT_ID:
+    raise ValueError(
+        'GOOGLE_OAUTH_CLIENT_ID is missing.' 
+        'Have you put it in a file at core/.env ?'
+    )
+
+# We need these lines below to allow the Google sign in popup to work.
+SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yourdomain.com']  # Add any necessary hosts
 
 import os
 from pathlib import Path
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100, help_text="Enter your name")
+    email = models.EmailField(max_length=100, help_text="Enter your email")
+    subject = models.CharField(max_length=100, help_text="Enter the subject")
+    message = models.TextField(help_text="Enter your message")
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.subject}'
