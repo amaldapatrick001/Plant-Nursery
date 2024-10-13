2e34e94956d412b3221fe6641d9a8a6529ecf4c9
Amalda Patrick <109305806+amaldapatrick001@users.noreply.github.com>
2024-10-14 00:58:21 +0530

    Add product updates

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from .models import Category, Product
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category_name', 'description')
-    search_fields = ('category_name',)
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'category', 'price', 'stock_quantity', 'status')
-    list_filter = ('category', 'sunlight_requirement', 'water_need', 'climate_compatibility')
-    search_fields = ('product_name', 'description')
