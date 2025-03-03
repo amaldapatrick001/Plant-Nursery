ecf5b34d6ab78d70a63e194840e9ec37aae6f59b
amaldapatrick001 <amaldapatrick@gmail.com>
2025-03-03 14:20:33 +0530

    Document updates

--- disease_detection/urls.py
+++
@@ -1,9 +0,0 @@
-# disease_detection/urls.py
-
-from django.urls import path
-from .views import PlantDiseaseDetectionView,upload_image_view
-app_name = 'disease_detection'
-urlpatterns = [
-    path('api/predict/', PlantDiseaseDetectionView.as_view(), name='predict_disease'),
-    path('upload/', upload_image_view, name='upload_image'),
-]
